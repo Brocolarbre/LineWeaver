7eed7fa318d30ea6c0be4b3f06e68ebb07328bb8
Brocolarbre <brocolarbre@gmail.com>
2026-07-23 11:18:05 +0200

    EaseCurve owns its interpolator

--- LineWeaver/include/LineWeaver/Easing/EaseCurve.hpp
+++ LineWeaver/include/LineWeaver/Easing/EaseCurve.hpp
@@ -8,11 +8,12 @@ namespace lw
 	class EaseCurve : public CloneableEasing<EaseCurve>
 	{
 	private:
-		const Interpolator& m_interpolator;
+		std::unique_ptr<Interpolator> m_interpolator;
 		Curve m_points;
 
 	public:
-		LINEWEAVER_EXPORT EaseCurve(const Interpolator& interpolator, const Curve& points);
+		LINEWEAVER_EXPORT EaseCurve(std::unique_ptr<Interpolator> interpolator, const Curve& points);
+		LINEWEAVER_EXPORT EaseCurve(const EaseCurve& easeCurve);
 
 		LINEWEAVER_EXPORT float operator()(float t) const override;
 	};
--- LineWeaver/src/Easing/EaseCurve.cpp
+++ LineWeaver/src/Easing/EaseCurve.cpp
@@ -2,15 +2,22 @@
 
 namespace lw
 {
-	EaseCurve::EaseCurve(const Interpolator& interpolator, const Curve& points) :
-		m_interpolator(interpolator),
+	EaseCurve::EaseCurve(std::unique_ptr<Interpolator> interpolator, const Curve& points) :
+		m_interpolator(std::move(interpolator)),
 		m_points(points)
+	{
+
+	}
+
+	EaseCurve::EaseCurve(const EaseCurve& easeCurve) :
+		m_interpolator(easeCurve.m_interpolator->clone()),
+		m_points(easeCurve.m_points)
 	{
 
 	}
 
 	float EaseCurve::operator()(float t) const
 	{
-		return m_interpolator(m_points, t).y;
+		return (*m_interpolator)(m_points, t).y;
 	}
 }
