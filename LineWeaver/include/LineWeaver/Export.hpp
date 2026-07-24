0721c9a92ad100a088f98c1e2ccd786e7d387a59
Brocolarbre <brocolarbre@gmail.com>
2026-07-24 16:09:27 +0200

    Updated template

--- LineWeaver/include/LineWeaver/Export.hpp
+++ LineWeaver/include/LineWeaver/Export.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#if defined(_WIN32)
-	#if defined(LINEWEAVER_STATIC)
-		#define LINEWEAVER_API
-	#elif defined(LINEWEAVER_BUILD)
-		#define LINEWEAVER_API __declspec(dllexport)
-	#else
-		#define LINEWEAVER_API __declspec(dllimport)
-	#endif
+#if defined(LINEWEAVER_STATIC)
+	#define LINEWEAVER_API
 #else
-	#define LINEWEAVER_API __attribute__((visibility("default")))
+	#if defined(_WIN32)
+		#if defined(LINEWEAVER_BUILD)
+			#define LINEWEAVER_API __declspec(dllexport)
+		#else
+			#define LINEWEAVER_API __declspec(dllimport)
+		#endif
+	#else
+		#define LINEWEAVER_API __attribute__((visibility("default")))
+	#endif
 #endif
